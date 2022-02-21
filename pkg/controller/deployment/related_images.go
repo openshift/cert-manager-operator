a0114bd517e2a1da8a6d639f87265217eab47326
Sebastian Laskawiec <slaskawi@redhat.com>
2022-02-21 09:17:45 +0100

    Support multiple image overrides

--- pkg/controller/deployment/related_images.go
+++ pkg/controller/deployment/related_images.go
@@ -1,20 +1,29 @@
 package deployment
 
-import "os"
-
-const (
-	environmentalVariableCertManagerRelatedImage = "RELATED_IMAGE_CERT_MANAGER"
+import (
+	"os"
+	"strings"
 )
 
-// Overrides provided image when environmentalVariableCertManagerRelatedImage environment variable is specified.
+var imageEnvMap = map[string]string{
+	"quay.io/jetstack/cert-manager-controller": "RELATED_IMAGE_CERT_MANAGER_CONTROLLER",
+	"quay.io/jetstack/cert-manager-webhook":    "RELATED_IMAGE_CERT_MANAGER_WEBHOOK",
+	"quay.io/jetstack/cert-manager-cainjector": "RELATED_IMAGE_CERT_MANAGER_CA_INJECTOR",
+}
+
+// Overrides provided image when envCertManagerControllerRelatedImage environment variable is specified.
 // Otherwise, returns provided defaultImage.
 //
 // This function assumes the Cert Manager is provided in all-in-one image. This function will need to be updated
 // is this assumption doesn't hold.
 func certManagerImage(defaultImage string) string {
-	env := os.Getenv(environmentalVariableCertManagerRelatedImage)
-	if env == "" {
-		return defaultImage
+	for image, env := range imageEnvMap {
+		if strings.Contains(defaultImage, image) {
+			overriddenImage := os.Getenv(env)
+			if overriddenImage != "" {
+				return overriddenImage
+			}
+		}
 	}
-	return env
+	return defaultImage
 }
