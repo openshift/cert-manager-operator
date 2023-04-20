e981ff02c7af9eb026d04132c4649d18365fff21
Swarup Ghosh <swghosh@redhat.com>
2023-04-20 16:16:22 +0530

    Validate pod labels hook to check supported controller pod labels
    Signed-off-by: Swarup Ghosh <swghosh@redhat.com>

--- pkg/controller/deployment/generic_deployment_controller.go
+++ pkg/controller/deployment/generic_deployment_controller.go
@@ -39,6 +39,7 @@ func newGenericDeploymentController(
 		[]deploymentcontroller.ManifestHookFunc{},
 		withOperandImageOverrideHook,
 		withPodLabelsOverrideHook(certManagerOperatorInformers.Operator().V1alpha1().CertManagers(), deployment.Name, getOverridePodLabelsFor),
+		withPodLabelsValidateHook(certManagerOperatorInformers.Operator().V1alpha1().CertManagers(), deployment.Name),
 		withContainerArgsOverrideHook(certManagerOperatorInformers.Operator().V1alpha1().CertManagers(), deployment.Name, getOverrideArgsFor),
 		withContainerArgsValidateHook(certManagerOperatorInformers.Operator().V1alpha1().CertManagers(), deployment.Name),
 		withContainerEnvOverrideHook(certManagerOperatorInformers.Operator().V1alpha1().CertManagers(), deployment.Name, getOverrideEnvFor),
