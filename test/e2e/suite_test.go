2dc12a38f050de6876bc8f5277b314db1ef06fb7
Swarup Ghosh <swghosh@redhat.com>
2023-08-21 17:24:19 +0530

    test/e2e: add ACME route53 dns01 cert test w/ and w/o explicit credentials - added ACME ingress http01 using ginkgo constructs
    Signed-off-by: Swarup Ghosh <swghosh@redhat.com>

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -4,6 +4,7 @@
 package e2e
 
 import (
+	"context"
 	"os"
 	"path/filepath"
 	"testing"
@@ -11,8 +12,11 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	certmanagerclientset "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned"
 	opv1 "github.com/openshift/api/operator/v1"
 	certmanoperatorclient "github.com/openshift/cert-manager-operator/pkg/operator/clientset/versioned"
+	"github.com/openshift/cert-manager-operator/test/library"
+	configv1 "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -37,7 +41,11 @@ var (
 	cfg          *rest.Config
 	k8sClientSet *kubernetes.Clientset
 
+	loader library.DynamicResourceLoader
+
+	configClient              *configv1.ConfigV1Client
 	certmanageroperatorclient *certmanoperatorclient.Clientset
+	certmanagerClient         *certmanagerclientset.Clientset
 
 	validOperatorStatusConditions = map[string]opv1.ConditionStatus{
 		"Available":   opv1.ConditionTrue,
@@ -78,17 +86,31 @@ var _ = BeforeSuite(func() {
 	cfg, err = config.GetConfig()
 	Expect(err).NotTo(HaveOccurred())
 
+	By("creating Kubernetes client set")
 	k8sClientSet, err = kubernetes.NewForConfig(cfg)
 	Expect(err).NotTo(HaveOccurred())
 
+	By("creating cert-manager operator client")
 	certmanageroperatorclient, err = certmanoperatorclient.NewForConfig(cfg)
 	Expect(err).NotTo(HaveOccurred())
 	Expect(certmanageroperatorclient).NotTo(BeNil())
 
+	By("verifying operator and cert-manager deployments status is available")
 	err = verifyOperatorStatusCondition(certmanageroperatorclient,
 		[]string{certManagerControllerDeploymentControllerName,
 			certManagerWebhookDeploymentControllerName,
 			certManagerCAInjectorDeploymentControllerName},
 		validOperatorStatusConditions)
 	Expect(err).NotTo(HaveOccurred(), "operator is expected to be available")
+
+	By("creating dynamic resources client")
+	loader = library.NewDynamicResourceLoader(context.TODO(), &testing.T{})
+
+	By("creating openshift config client")
+	configClient, err = configv1.NewForConfig(cfg)
+	Expect(err).NotTo(HaveOccurred())
+
+	By("creating cert-manager client")
+	certmanagerClient, err = certmanagerclientset.NewForConfig(cfg)
+	Expect(err).NotTo(HaveOccurred())
 })
--- test/e2e/utils_test.go
+++ test/e2e/utils_test.go
@@ -5,24 +5,48 @@ package e2e
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"sync"
 	"time"
 
+	cmv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmmetav1 "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 	opv1 "github.com/openshift/api/operator/v1"
 
 	"github.com/openshift/cert-manager-operator/api/operator/v1alpha1"
 	certmanoperatorclient "github.com/openshift/cert-manager-operator/pkg/operator/clientset/versioned"
+	"github.com/openshift/cert-manager-operator/test/library"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/util/retry"
 )
+
+var defaultCertManagerState = v1alpha1.CertManager{
+	ObjectMeta: v1.ObjectMeta{
+		Name: "cluster",
+	},
+	Spec: v1alpha1.CertManagerSpec{
+		OperatorSpec: opv1.OperatorSpec{
+			ManagementState: opv1.Managed,
+		},
+	},
+}
+
+var subscriptionSchema = schema.GroupVersionResource{
+	Group:    "operators.coreos.com",
+	Version:  "v1alpha1",
+	Resource: "subscriptions",
+}
 
 // verifyOperatorStatusCondition polls every 1 second to check if the status of each of the controllers
 // match with the expected conditions. It returns an error if a timeout (5 mins) occurs or an error was
@@ -165,5 +189,93 @@ func verifyDeploymentArgs(k8sclient *kubernetes.Clientset, deploymentName string
 		}
 
 		return true, nil
+	})
+}
+
+// getCertManagerOperatorSubscription returns the name of the first subscription object by listing
+// them in the cert-manager-operator namespace using a k8s dynamic client
+func getCertManagerOperatorSubscription(ctx context.Context, loader library.DynamicResourceLoader) (string, error) {
+	subscriptionClient := loader.DynamicClient.Resource(subscriptionSchema).Namespace("cert-manager-operator")
+
+	subs, err := subscriptionClient.List(ctx, v1.ListOptions{})
+	if err != nil {
+		return "", err
+	}
+	if len(subs.Items) == 0 {
+		return "", fmt.Errorf("no subscription object found in operator namespace")
+	}
+
+	subName, ok := subs.Items[0].Object["metadata"].(map[string]interface{})["name"].(string)
+	if !ok {
+		return "", fmt.Errorf("could not parse metadata.name from the first subscription object found")
+	}
+	return subName, nil
+}
+
+// patchSubscriptionWithCloudCredential uses the k8s dynamic client to patche the only Subscription object
+// in the cert-manager-operator namespace to inject CLOUD_CREDENTIALS_SECRET_NAME="aws-creds" env
+// into its spec.config.env
+func patchSubscriptionWithCloudCredential(ctx context.Context, loader library.DynamicResourceLoader) error {
+	subName, err := getCertManagerOperatorSubscription(ctx, loader)
+	if err != nil {
+		return err
+	}
+
+	patch := map[string]interface{}{
+		"spec": map[string]interface{}{
+			"config": map[string]interface{}{
+				"env": []interface{}{
+					map[string]interface{}{
+						"name":  "CLOUD_CREDENTIALS_SECRET_NAME",
+						"value": "aws-creds",
+					},
+				},
+			},
+		},
+	}
+	payload, err := json.Marshal(patch)
+	if err != nil {
+		return err
+	}
+
+	subscriptionClient := loader.DynamicClient.Resource(subscriptionSchema).Namespace("cert-manager-operator")
+	_, err = subscriptionClient.Patch(ctx, subName, types.MergePatchType, payload, v1.PatchOptions{})
+	return err
+}
+
+// waitForCertificateReadiness polls the status of the Certificate object and returns non-nil error
+// once the Ready condition is true, otherwise should return a time-out error
+func waitForCertificateReadiness(ctx context.Context, certName, namespace string) error {
+	return wait.PollImmediate(PollInterval, TestTimeout, func() (bool, error) {
+		cert, err := certmanagerClient.CertmanagerV1().Certificates(namespace).Get(ctx, certName, metav1.GetOptions{})
+		if err != nil {
+			return false, nil
+		}
+
+		for _, cond := range cert.Status.Conditions {
+			if cond.Type == cmv1.CertificateConditionReady {
+				return cond.Status == cmmetav1.ConditionTrue, nil
+			}
+		}
+		return false, nil
+	})
+}
+
+// verifyCertificate loads the tls secret as a X509 certificate and verifies the following
+// - certificate secret is non null, i.e. secret contains "tls.crt", "tls.key" keys
+// - certificate hasn't expired
+// - certificate has subject CN matching provided hostname
+func verifyCertificate(ctx context.Context, secretName, namespace, hostname string) error {
+	return wait.PollImmediate(PollInterval, TestTimeout, func() (bool, error) {
+		secret, err := loader.KubeClient.CoreV1().Secrets(namespace).Get(ctx, secretName, metav1.GetOptions{})
+		if err != nil {
+			return false, nil
+		}
+
+		isVerified, err := library.VerifyCertificate(secret, hostname)
+		if err != nil {
+			return false, err
+		}
+		return isVerified, nil
 	})
 }
