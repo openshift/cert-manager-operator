638d756434c72bf96478fe344df36670d2865594
siddhi bhor <sbhor@redhat.com>
2025-04-30 18:30:41 +0530

    updates for tools.go and openapi.json

--- hack/update-clientgen.sh
+++ hack/update-clientgen.sh
@@ -19,6 +19,7 @@ for group in ${API_GROUP_VERSIONS};  do
       --with-applyconfig \
       --applyconfig-name "applyconfigurations" \
       --applyconfig-externals "github.com/openshift/api/operator/v1.OperatorSpec:github.com/openshift/client-go/operator/applyconfigurations/operator/v1,github.com/openshift/api/operator/v1.OperatorStatus:github.com/openshift/client-go/operator/applyconfigurations/operator/v1,github.com/openshift/api/operator/v1.OperatorCondition:github.com/openshift/client-go/operator/applyconfigurations/operator/v1,github.com/openshift/api/operator/v1.GenerationStatus:github.com/openshift/client-go/operator/applyconfigurations/operator/v1" \
+      --applyconfig-openapi-schema "vendor/github.com/openshift/api/openapi/openapi.json" \
       --one-input-api "${group}" \
       --output-pkg github.com/openshift/cert-manager-operator/pkg/"${group%\/*}" \
       --output-dir "${SCRIPT_ROOT}/pkg/${group}" \
--- tools/tools.go
+++ tools/tools.go
@@ -22,6 +22,7 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
+	_ "github.com/openshift/api/openapi"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v5"
