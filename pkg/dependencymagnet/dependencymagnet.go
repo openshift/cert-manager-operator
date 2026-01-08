79f3a3b2d4f3a33b4ad71e959730ebafac155e77
chiragkyal <ckyal@redhat.com>
2026-01-08 14:03:45 +0530

    Segregate go packages into separate go.mod
    Signed-off-by: chiragkyal <ckyal@redhat.com>

--- pkg/dependencymagnet/dependencymagnet.go
+++ pkg/dependencymagnet/dependencymagnet.go
@@ -1,12 +1,12 @@
 //go:build dependencymagnet
 // +build dependencymagnet
 
-package tools
+package dependencymagnet
 
 import (
 	_ "github.com/go-bindata/go-bindata/go-bindata"
 	_ "github.com/google/go-jsonnet/cmd/jsonnet"
-	_ "github.com/mogensen/kubernetes-split-yaml"
+	_ "github.com/openshift/api/openapi"
 	_ "github.com/openshift/build-machinery-go"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo"
--- tools/tools.go
+++ tools/tools.go
@@ -22,7 +22,6 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
-	_ "github.com/openshift/api/openapi"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v5"
